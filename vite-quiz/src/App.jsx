dacc3e9f9b8fa9ca457f1fb952d75bacad67d811
gomsik811 <gomsik811@gmail.com>
2026-06-11 14:56:30 +0900

    리엑트 퀴즈O 프로젝트 생성, 화면 텍스트

---
+++ vite-quiz/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Quiz01 from './quiz/Quiz01'
+
+function App() {
+
+  return(
+  <Quiz01/>  
+  )
+}
+
+export default App
---
+++ vite-quiz/src/quiz/Quiz01.jsx
@@ -0,0 +1,12 @@
+import './Quiz01.css'
+
+function Quiz01() {
+return(
+    <div className="bc-color">
+        <h1 className="font-white">React 프로젝트</h1>
+        <h3 className="font-white">각자 이름</h3>
+    </div>
+)
+
+}
+export default Quiz01;
