2a3c516dbcc80e1603dfb837aa179bf7bce298fe
gomsik811 <gomsik811@gmail.com>
2026-06-11 16:44:36 +0900

    리엑트 js 데이터 export import

--- react-study/src/App.jsx
+++ react-study/src/App.jsx
@@ -5,6 +5,8 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Basic01 from './components/Basic01'
 import State01 from './components/State01'
+import Export01 from './components/Export01'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +14,8 @@ function App() {
   return (
     
       // <Basic01/>
-      <State01/>
+      //<State01/>
+      <Export01/>
   
   )
 }
