b15e290216c40294c79e0686d6d4e9bef560e0c1
gomsik811 <gomsik811@gmail.com>
2026-06-17 15:06:35 +0900

    경로별 페이지처리 (react-router) 설치 및 설정 적용 페이지 단위 폴더 분할

--- react-study/src/foodmarket/FoodMarket.jsx
+++ react-study/src/foodmarket/FoodMarket.jsx
@@ -15,11 +15,26 @@ import foodsData from './data/foodsData';
 import { useState } from 'react';
 import FoodCard from './components/FoodCard';
 
+import { Routes, Route, Link, useNavigate } from 'react-router';
+import CustomerService from './pages/CustomerService';
+import Home from './pages/Home';
+
 
 
 function FoodMarket() {
 
     let [foods, setFoods] = useState(foodsData);
+
+    // 기본 경로이동
+    // <a href="주소"> 새로운 주소로 이동 (새로고침)
+    // location.href = '주소
+
+    // react-router
+    // Lunk to=주소
+    // navigate(주소)
+
+    let navigate = useNavigate();
+    
 
     return (
         <div>
@@ -28,98 +43,26 @@ function FoodMarket() {
                 <Container>
                     <Navbar.Brand href="#home">FoodMarket</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#home"><Link to="/">Home</Link></Nav.Link>
+                        <Nav.Link onClick={()=>(navigate("/detail"))}>FoodDetail</Nav.Link>
+                        <Nav.Link onClick={()=>(navigate("/info"))}>Info</Nav.Link>
+                        <Nav.Link onClick={()=>(navigate("/help"))}>고객센터</Nav.Link>                   
+
+                        {/* <Nav.Link href="#features"><Link to="/detail">FoodDetail</Link></Nav.Link> */}
+                        {/* <Nav.Link href="/info">Info</Nav.Link>
+                        <Nav.Link href="/help">고객센터</Nav.Link> */}
                     </Nav>
                 </Container>
             </Navbar>
 
-            {/*
-            이미 사용
+            <Routes>
+                <Route path="/" element={<Home foods={foods} />} />
+                <Route path="/help" element={<CustomerService />} />
+                <Route path="/detail" element={<div><h1>detail page</h1></div>} />
+                <Route path="/info" element={<div><h1>info page</h1></div>} />
+                <Route path="/*" element={<div><h1>잘못된 접근입니다.</h1></div>} />
 
-            1. css 에서 사용
-                이미지 경로 (상대경로 ./img/...)
-
-            2. js(jsx) 에서 사용
-
-            상대경로 연결사용 XXX
-            <img src="../../imgage/menu.png"/> XXX
-
-            react 컴포넌트에서 이미지 사용시, import -> 사용
-            <img src="https://...외부url"/>            
-            <img src={import한이미지}/>
-
-            3. public 폴더에 이미지를 미리 저장해놓고 사용
-            현재 실행중인 서버가 public 경로를 통해 image에 접근할 수 있는 서버 역할
-            이미지 외부url에 접근해서 사용하듯이...사용
-
-            <img src={'http://localhost:5173/images/food1.jpg'}/>
-            <ing src={'/images/food1/jpg'}/>
-
-            <img src={'http://localhost:5173/images/food1.jpg'}/>
-            <img src={'http://hellofoodmarket.com/images/food1.jpg'} />
-
-
-            CRA:    :   <img src={ process.env.PUBLIC_URL + '/images/food1.jpg}/>
-                        packge.json
-                        "homepage":"/"detailServicePath"  상세경로설정
-            Vite    :   <img src={ import.meta.env.BASE_URL + '/images/food1.jpg}/>  
-                        vite.config.js
-
-        */}
-
-
-            {/* <img src={banner_bg} /> */}
-            {/* <div className='main-bg' style={{backgroundImage:'url(' + banner_bg + ')'}}></div> */}
-            <div className='main-bg'></div>
-
-
-
-
-            {/*
-                    bootstrap 한줄 기준 : 12
-                */}
-            <Container>
-                <Row>
-                    {                   
-                            foods.map((food, index) => {
-                                return (
-                                    <Col md={4} sm={2}>
-                                        {/*<foodcard  id={food.id} title={food.title} /> */}
-                                        <FoodCard food={food} foods={foods} index={index} />
-                                    </Col>
-                                )
-                            })
-                    }
-                </Row>
-            </Container>
-
-
-
-
-
-
-
-
-
-
-            {/* <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>
-
-            <Button variant="primary">Primary</Button>
-            <Button variant="secondary">Secondary</Button>
-            <Button variant="success">Success</Button>
-            <Button variant="warning">Warning</Button>
-            <Button variant="danger">Danger</Button>
-            <Button variant="info">Info</Button>
-            <Button variant="light">Light</Button>
-            <Button variant="dark">Dark</Button>
-            <Button variant="link">Link</Button> */}
-
-
-
+            </Routes>
 
 
         </div>
